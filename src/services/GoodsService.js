17af29b0c3b5b9b3310b2861f42a2195a563e649
levandung-it03 <levandung.it03@gmail.com>
2024-11-29 11:48:03 +0700

    Rename 'helpers' folder

--- src/services/GoodsService.js
+++ src/services/GoodsService.js
@@ -1,5 +1,5 @@
 import { UtilAxios } from "@reusable/Utils";
-import { springService } from "@src/helpers/AxiosConfig";
+import { springService } from "@src/configs/AxiosConfig";
 
 const USER_PREFIX_PART = process.env.REACT_APP_SPRING_USER_PREFIX_PART;
 
