8b71b6cb0d51dffeb8853d06c1263da3344dba55
levandung-it03 <levandung.it03@gmail.com>
2024-12-01 23:21:43 +0700

    Pull Request

--- src/pages/Client/ManageSupplier/ManageSupplier.js
+++ src/pages/Client/ManageSupplier/ManageSupplier.js
@@ -1,5 +1,64 @@
+import { useMemo, useState } from "react";
 import "./ManageSupplier.scss";
+import { FormatterDict, Table } from "@reusable/TableHMCompos/TableHMCompos";
+import { InputBuilder, SelectBuilder } from "@reusable/FormHMCompos/FormHMCompos";
+import Dialog from "@reusable/Dialog/Dialog";
+import { UserSupplierService } from "@services/SupplierService";
+import { UtilMethods } from "@reusable/Utils";
 
 export default function ManageSupplier() {
-    
+    const [dialogProps, setDialogProps] = useState({ isOpen: false, title: '', body: null });
+
+    const tableComponents = useMemo(() => FormatterDict.TableComponents({
+        tableInfo: {
+            title: "Manage Supplier Table",
+            primaryKeyName: "supplierId",
+            columnsInfo: [
+                FormatterDict.ColumnInfo('supplierId', 'Supplier Id', { name: "supplierId", builder: InputBuilder({ type: "text", readOnly: true }) }),
+                FormatterDict.ColumnInfo('supplierName', 'Supplier Name', { name: "supplierName", builder: InputBuilder({ type: "text", validators: [
+                    v => !UtilMethods.checkIsBlank(v) || "Value is required",
+                ] }) }),
+                FormatterDict.ColumnInfo('supplierName', 'Supplier Name', { name: "supplierName",
+                    builder: InputBuilder({ type: "text", readOnly: true }) }),
+            ],
+            filterFields: [
+                FormatterDict.FilterField('supplierId', 'Supplier Id', InputBuilder({ type: "number" })),
+                FormatterDict.FilterField('supplierName', 'Supplier Name', InputBuilder({ type: "text" })),
+            ],
+            sortingFields: [
+                FormatterDict.SortingField('supplierId', 'Supplier Id'),
+                FormatterDict.SortingField('supplierName', 'Supplier Name'),
+            ]
+        },
+        apiServices: {
+            GET_service: { action: UserSupplierService.getSupplierPages },
+            UPDATE_service: { action: UserSupplierService.updateSupplier },
+        },
+    }), []);
+
+    const addingFormComponents = useMemo(() => FormatterDict.AddingFormComponents({
+        apiServices: {
+            POST_service: { action: UserSupplierService.addSupplier },
+        },
+        childrenBuildersInfo: [
+            { name: "supplierName", builder: InputBuilder({ type: "text", validators: [
+                v => !UtilMethods.checkIsBlank(v) || "Value is required",
+            ] }) },
+        ],
+    }), []);
+
+    const contextMenuComponents = useMemo(() => FormatterDict.ContextMenuComponents([
+    ]), []);
+
+    return (
+        <div className="manage-supplier">
+            <Table
+                tableComponents={tableComponents}
+                addingFormComponents={addingFormComponents}
+                contextMenuComponents={contextMenuComponents}
+                tableModes={FormatterDict.TableModes(true, true, true, true, true)}
+            />
+            <Dialog dialogProps={dialogProps} setDialogProps={setDialogProps} />
+        </div>
+    );
 }
--- src/services/SupplierService.js
+++ src/services/SupplierService.js
@@ -4,5 +4,36 @@ import { springService } from "@src/configs/AxiosConfig";
 const USER_PREFIX_PART = process.env.REACT_APP_SPRING_USER_PREFIX_PART;
 
 export class UserSupplierService {
+    static async getSupplierPages({ page, filterFields, sortedField, sortedMode }) {
+        try {
+            const response = await springService.get(`${USER_PREFIX_PART}/v1/get-suppliers-pages`, {
+                params: { page, filterFields, sortedField, sortedMode },
+                paramsSerializer: UtilAxios.paramsSerializerToGetWithSortAndFilter,
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error.response ? error.response.data : error;
+        }
+    }
+
+    static async updateSupplier(formData) {
+        try {
+            const response = await springService.put(`${USER_PREFIX_PART}/v1/update-supplier`, formData);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error.response ? error.response.data : error;
+        }
+    }
     
+    static async addSupplier(formData) {
+        try {
+            const response = await springService.post(`${USER_PREFIX_PART}/v1/add-supplier`, formData);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error.response ? error.response.data : error;
+        }
+    }
 }
